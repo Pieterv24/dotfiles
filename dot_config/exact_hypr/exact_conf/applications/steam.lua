68b75403a9256b7442c9049dc378bef703766ae1
Pieterv24 <git@pieterv24.dev>
2026-08-06 14:54:02 +0200

    update

--- dot_config/exact_hypr/exact_conf/applications/steam.lua
+++ dot_config/exact_hypr/exact_conf/applications/steam.lua
@@ -1,3 +1,5 @@
 hl.window_rule({ float = true, opacity = "1 1", idle_inhibit = "fullscreen", match = { class = "steam" } })
 hl.window_rule({ center = true, size = "1100 700", match = { class = "steam", title = "Steam" } })
 hl.window_rule({ size = "460 800", match = { class = "steam", title = "Friends List" } })
+
+hl.window_rule({ workspace = 10, tile = true, match = { class = "^(steam_app_.*)$" } })
